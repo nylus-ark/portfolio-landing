b5bb821d7e2423763c8c09868bd0176b3ad7763d
Pavel <krasulinpavel1@yandex.ru>
2023-07-10 00:02:22 +0400

    Add header widget and hireButton feature

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import GoogleFontLoader from 'react-google-font-loader';
+
 import './styles/index.css';
 import './styles/variables.css';
+
 import { HomePage } from './pages/homePage/homePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
+        <GoogleFontLoader
+            fonts = {[
+                {
+                    font: 'Inter',
+                    weights: [
+                        400, 
+                        500, 
+                        700,
+                    ],
+                },
+            ]}
+        />
         <HomePage />
     </React.StrictMode>
 );
