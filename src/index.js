14d193011889fb3450f3ab893772fa2ece43e208
Pavel <krasulinpavel1@yandex.ru>
2023-07-29 23:11:29 +0400

    Add fonts with @font-face

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GoogleFontLoader from 'react-google-font-loader';
 
 import './styles/index.css';
 import './styles/variables.css';
+import './styles/fonts.css';
 
 import { HomePage } from './pages/homePage/homePage';
 
@@ -11,18 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <GoogleFontLoader
-            fonts = {[
-                {
-                    font: 'Inter',
-                    weights: [
-                        400, 
-                        500, 
-                        700,
-                    ],
-                },
-            ]}
-        />
         <HomePage />
     </React.StrictMode>
 );
